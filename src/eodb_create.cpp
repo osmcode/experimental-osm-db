065b8871b89b6f2ecf32f47db559b49ec8f691af
Jochen Topf <jochen@topf.org>
2018-01-03 17:10:03 +0100

    Register maps for storing offsets.

--- src/eodb_create.cpp
+++ src/eodb_create.cpp
@@ -66,6 +66,30 @@ typedef osmium::index::map::Map<osmium::unsigned_object_id_type, osmium::Locatio
 
 typedef AnyMultimap<osmium::unsigned_object_id_type, osmium::unsigned_object_id_type> map_type;
 
+#ifdef OSMIUM_HAS_INDEX_MAP_DENSE_FILE_ARRAY
+    REGISTER_MAP(osmium::unsigned_object_id_type, size_t, osmium::index::map::DenseFileArray, dense_file_array)
+#endif
+
+#ifdef OSMIUM_HAS_INDEX_MAP_SPARSE_FILE_ARRAY
+    REGISTER_MAP(osmium::unsigned_object_id_type, size_t, osmium::index::map::SparseFileArray, sparse_file_array)
+#endif
+
+#ifdef OSMIUM_HAS_INDEX_MAP_SPARSE_MEM_ARRAY
+    REGISTER_MAP(osmium::unsigned_object_id_type, size_t, osmium::index::map::SparseMemArray, sparse_mem_array)
+#endif
+
+#ifdef OSMIUM_HAS_INDEX_MAP_SPARSE_MEM_MAP
+    REGISTER_MAP(osmium::unsigned_object_id_type, size_t, osmium::index::map::SparseMemMap, sparse_mem_map)
+#endif
+
+#ifdef OSMIUM_HAS_INDEX_MAP_SPARSE_MEM_TABLE
+    REGISTER_MAP(osmium::unsigned_object_id_type, size_t, osmium::index::map::SparseMemTable, sparse_mem_table)
+#endif
+
+#ifdef OSMIUM_HAS_INDEX_MAP_SPARSE_MMAP_ARRAY
+    REGISTER_MAP(osmium::unsigned_object_id_type, size_t, osmium::index::map::SparseMmapArray, sparse_mmap_array)
+#endif
+
 class Options : public OptionsBase {
 
     std::string m_index_type{"sparse_mem_array"};
